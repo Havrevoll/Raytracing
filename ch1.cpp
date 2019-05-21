4376926bdc36be3225b630effed7a87f70587725
Ola Haugen Havrevoll <havrevoll@gmail.com>
2019-05-22 00:15:03 +0200

    Fyrste

---
+++ ch1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main() 
+{
+
+
+	int nx; //= 200;
+	int ny;// = 100;
+	std::cout << "nx = ";
+	std::cin >> nx;
+	std::cout << "ny = ";
+	std::cin >> ny;
+
+
+	int n = 3;
+
+	unsigned char  data [nx*ny*n] = {};
+	
+	for (int j = ny-1; j >= 0; j--) {
+		for (int i = 0; i<nx;i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99*r);
+			int ig = int(255.99*g);
+			int ib = int(255.99*b);
+
+			int k = ( (ny-1) - j) * nx * n + i*n ; 
+			data[ k  ] = ir;
+			data[  k +1] = ig;
+			data[  k +2] = ib;
+
+			std::cout << "j = " << j << ", i = " << i <<", k = " << k << std::endl; 
+		}
+	}
+	
+	stbi_write_png("bilete.png", nx,ny, n, data, 0);
+
+
+
+}
