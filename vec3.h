a1d36bb07d77d6a0fc967e9fde04312779f2fb98
Ola Haugen Havrevoll <havrevoll@gmail.com>
2019-05-29 08:18:00 +0200

    Har byrja på ray

--- vec3.h
+++ vec3.h
@@ -17,21 +17,21 @@ using std::cout;
 class vec3
 {
 	public:
-		double e[3];
+		float e[3];
 
 		vec3() { e[0] = e[1] = e[2] = 0.0; }
-		vec3(double n1, double n2, double n3) {
+		vec3(float n1, float n2, float n3) {
 			e[0] = n1;
 			e[1] = n2;
 			e[2] = n3;
 		}
 		// ~vec3();
-		double x() const {return e[0];}
-		double y() const {return e[1];}
-		double z() const {return e[2];}
-		double r() const {return e[0];}
-		double g() const {return e[1];}
-		double b() const {return e[2];}
+		float x() const {return e[0];}
+		float y() const {return e[1];}
+		float z() const {return e[2];}
+		float r() const {return e[0];}
+		float g() const {return e[1];}
+		float b() const {return e[2];}
 
 
 
@@ -40,31 +40,31 @@ class vec3
 		//vec3 operator-(const vec3 & b) const;
 		vec3 operator-() const { return vec3(-e[0],-e[1],-e[2]); }
 		//vec3 operator*(const vec3 & b) const;//Burde ikkje dette vera prikkproduktet? Er det nokon gong aktuelt å elementvis multiplisera to vektorar?
-		//vec3 operator*(double n) const;
-		//vec3 operator/(double n) const;
+		//vec3 operator*(float n) const;
+		//vec3 operator/(float n) const;
 
-		double operator[](int i) const { return e[i]; }
-		double & operator[](int i) { return e[i]; }
+		float operator[](int i) const { return e[i]; }
+		float & operator[](int i) { return e[i]; }
 
 
 		vec3 & operator+=(const vec3 & b);
 		vec3 & operator-=(const vec3 & b);
 		vec3 & operator*=(const vec3 & b);
 		vec3 & operator/=(const vec3 & b);
-		vec3 & operator*=(const double n);
-		vec3 & operator/=(const double n);
+		vec3 & operator*=(const float n);
+		vec3 & operator/=(const float n);
 
 		// Vektor-operasjonar som ikkje er så vanlege
-		//double dot(const vec3 & v1, const vec3 & v2);
+		//float dot(const vec3 & v1, const vec3 & v2);
 		//vec3 cross(const vec3 & v1, const vec3 & v2);
 
 		void make_unit_vector();
 		//vec3 unit(vec3 v);
-		double length();
-		double len() {return length(); }
+		float length();
+		float len() {return length(); }
 
 
-		//friend vec3 operator*(double n, const vec3 & a);
+		//friend vec3 operator*(float n, const vec3 & a);
 		//friend std::ostream & operator<<(std::ostream & os, const vec3 & v);
 		//friend std::istream & operator>>(std::istream & is, const vec3 & v); 
 
@@ -87,17 +87,17 @@ vec3 operator*(const vec3 & a, const vec3 & b)
 	return vec3(a.e[0] * b.e[0], a.e[1] * b.e[1], a.e[2] * b.e[2]);
 }
 
-vec3 operator*(const vec3 & a, double n) 
+vec3 operator*(const vec3 & a, float n) 
 {
 	return vec3(a.e[0]*n, a.e[1]*n, a.e[2]*n );
 }
 
-vec3 operator*(double n, const vec3 & a)
+vec3 operator*(float n, const vec3 & a)
 {
 	return vec3(a.e[0]*n, a.e[1]*n, a.e[2]*n );
 }
 
-vec3 operator/(const vec3 & a, double n)
+vec3 operator/(const vec3 & a, float n)
 {
 	return vec3(a.e[0]/n, a.e[1]/n, a.e[2]/n );
 }
@@ -133,7 +133,7 @@ vec3 & vec3::operator/=(const vec3 & b)
 	return *this;
 }
 
-vec3 & vec3::operator*=(const double n)
+vec3 & vec3::operator*=(const float n)
 {
 	e[0] *= n;
 	e[1] *= n;
@@ -141,7 +141,7 @@ vec3 & vec3::operator*=(const double n)
 	return *this;
 }
 
-vec3 & vec3::operator/=(const double n)
+vec3 & vec3::operator/=(const float n)
 {
 	e[0] /= n;
 	e[1] /= n;
@@ -150,7 +150,7 @@ vec3 & vec3::operator/=(const double n)
 }
 
 
-double dot(const vec3 & v1, const vec3 & v2)
+float dot(const vec3 & v1, const vec3 & v2)
 {
 	return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] +v1.e[2] * v2.e[2] ;
 }
@@ -162,16 +162,19 @@ vec3 cross(const vec3 & a, const vec3 & b)
 
 void vec3::make_unit_vector()
 {
-	double k = 1.0 / ( sqrt(e[0]*e[0] + e[1] * e[1] + e[2]*e[2]) );
+	float k = 1.0 / ( sqrt(e[0]*e[0] + e[1] * e[1] + e[2]*e[2]) );
 	e[0] *= k; e[1] *= k; e[2] *= k;
 }
+
 
 vec3 unit(vec3 v)
 {
 	return v / v.length();
 }
 
-double vec3::length()
+vec3 unit_vector(vec3 v) { return unit(v);}
+
+float vec3::length()
 {
 	return sqrt(e[0]*e[0] + e[1] * e[1] + e[2]*e[2]);
 }
